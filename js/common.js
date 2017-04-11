5515b9bbcf211c550eeec177f77892bbdf0e0aa5
EBA-PC\EBA <hatakou9169@gmail.com>
2017-04-11 15:11:59 +0900

    サイトデータを全て移行

---
+++ js/common.js
@@ -0,0 +1,14 @@
+function writeHeader(rootDir){
+
+	$.ajax({
+		url: rootDir + "/header.html",
+		cache: false,
+		async: false,
+		success: function(html){
+
+			html = html.replace(/\{\$root\}/g, rootDir);
+			document.write(html);
+		}
+	});
+
+}
